f438b4af0a7d19dca04ee7fd26f4b9db02454f2b
Alberto Zubiri <azubiri@protonmail.com>
2020-04-11 11:41:34 +0200

    some corrections in comments

--- src/main.cpp
+++ src/main.cpp
@@ -12,24 +12,24 @@ DigitalIn mybutton(USER_BUTTON);
 // To print readings from sensor via USB easily. UART pin names are required (see PinNames.h)
 Serial pc(SERIAL_TX, SERIAL_RX);
 
-// 7 bits where slave address is allocated (8th bit of uint8_t is 0)
+// 7 LSbits where slave address is allocated (8th bit of uint8_t is 0)
 static const uint8_t TMP275_ADDR = 0x48;
-// 7 bits where slave address and 1 bit for reading are allocated 
+// 7 MSbits where slave address and 1 LSbit for reading are allocated 
 static const uint8_t TMP275_R_ADDR = TMP275_ADDR << 1;
-// 7 bits where slave address and 1 bit for writing are allocated 
+// 7 MSbits where slave address and 1 LSbit for writing are allocated 
 static const uint8_t TMP275_W_ADDR = TMP275_R_ADDR | 1;
 // Pointer regiter addresses
 static const uint8_t REG_TEMP = 0x00; // Temperature Register Address
 static const uint8_t REG_CONF = 0x01; // Configuraton Register Address
 
 int main(){
-    // Buffer where data packet will be stored
+    // Buffer where data package will be stored
     char buf[2];
     // 2 bytes where temperature readings will be allocated properly
     int16_t val;
     // Temperature in degrees
     float temp_c;
-    // Acknowledment between board and sensor communication
+    // Acknowledgement between board and sensor communication
     int ack;
     // Time in seconds between readings
     int8_t time = 1;
@@ -43,14 +43,14 @@ int main(){
     // ----------------------------------------
     
     // Now pointer register will be as configuration register:
-    // Pointer address byte is stored in the FIRST byte of the buffer
+    // Pointer address byte is stored in the LSbyte of the buffer
     buf[0] = REG_CONF;
     pc.printf("Current pointer address: 0x%02x\n\r", buf[0]);
     // Set the pointer address byte as a configuration register address byte
     ack = i2c.write(TMP275_W_ADDR, &buf[0], 1);
     
     // Now the current configuration will be read:
-    // Configuration register format will be stored in the SECOND byte of the buffer
+    // Configuration register format will be stored in the MSbyte of the buffer
     //
     // Summary about what is the meaning of each bit of this byte:
     // Bit0: ShutDown Mode SD: allows saving maximum power
@@ -61,24 +61,22 @@ int main(){
     // Bit5: Converter Resolution R0: Resolution and converstion time
     // Bit6: Converter Resolution R1: Resolution and conversion time
     // Bit7: One-Shot OS: Single temperature conversion
-    //
-    // Example: If we obtain in buf[1]:
-    // buf[1] = Bit7Bit6Bit5Bit4Bit3Bit2Bit1Bit0 = 01100000 = 0x60
     // "More details in the TMP275 Datasheet"
     ack = i2c.read(TMP275_R_ADDR, &buf[1], 1);
     pc.printf("Current configuration of the sensor: 0x%02x, ACK: %d\n\r", buf[1], ack);
     
-    // Here the configuration can be changed
+    // Here the configuration frame will be changed 
+    // (or not, since it is depending on the current configuration read before)
     buf[1] = 0x60;
-    // When master(board) is writing needs always include poiter address before configuration frame
-    // Address+Writing+P.Register+Conf.Format bits are sent by the board to TMP275,
-    // and Acknowledge frame is sent by TMP275 to the board
+    // When master(board) is writing, it always needed to include a poiter address before configuration frame
+    // Address+Writing+P.Register+Conf.Format: These bits are sent by the board to TMP275,
+    // and an Acknowledge frame is sent by TMP275 to the board
     ack = i2c.write(TMP275_W_ADDR, buf, 2);
     
-    // To make sure the configuration once changes are done
-    // To read the configuration, pointer register is not needed in this case
-    // Address+Reading+Conf.Format bits are sent by the board to TMP275,
-    // Configuration frame will be allocated in SECOND frame,
+    // To make sure the configuration once changes are done:
+    // Read the configuration, and pointer register is not needed in this case
+    // Address+Reading+Conf.Format: Thses bits are sent by the board to TMP275,
+    // Configuration frame will be allocated in the MSbyte of the buffer,
     // and Acknowledge frame is sent by TMP275 to the board
     ack = i2c.read(TMP275_R_ADDR, &buf[1], 1);
     pc.printf("Configuration of the sensor after changes: 0x%02x, ACK: %d\n\r", buf[1], ack);
@@ -90,33 +88,49 @@ int main(){
     buf[0] = REG_TEMP;
     pc.printf("Current pointer address: 0x%02x\n\r", buf[0]);
     ack = i2c.write(TMP275_W_ADDR, &buf[0], 1);
-    pc.printf("Current pointer addresssssss: 0x%02x\n\r", buf[0]);
+    pc.printf("Reply of the writing: 0x%02x\n\r", buf[0]);
+
     // If we want to stop this program, press the USER_BUTTON
     while(mybutton){
         if(!ack){
             // Here temperature readings
-            // Temperature will be stored in 2 bytes. So, they will be combined.
+            // Temperature will be stored in 2 bytes.
+            // So, a shifting and a combination of both will be needed to know temperature value.
             ack = i2c.read( TMP275_R_ADDR, buf, 2);
-            // First byte: All bits have information about a part of the reading.
-            // But we need to free up space for information of the second byte.
-            pc.printf("Byte 0: 0x%x\n\r", buf[0]);
-            pc.printf("Byte 0: 0x%x\n\r", buf[0] << 4);
-            // Second byte: The 4 lasts bits are not useful, so they will be removed
-            // and 4 firsts bits will be moved to 4 bits to right
-            pc.printf("Byte 1: 0x%x\n\r", buf[1]);
-            pc.printf("Byte 1: 0x%x\n\r", buf[1] >> 4);
-            //Combine both bytes: 
+            // LSbyte: All bits have information about a part of the reading.
+            // But we need to free up space for information of the MSbyte.
+            pc.printf("LSbyte: 0x%x\n\r", buf[0]);
+            pc.printf("LSbyte: 0x%x\n\r", buf[0] << 4);
+            // MSbyte: The 4 LSbits are not useful, therefore they will be removed
+            // by a shifting of 4 MSbits from left to right
+            pc.printf("MSbyte: 0x%x\n\r", buf[1]);
+            pc.printf("MSbyte: 0x%x\n\r", buf[1] >> 4);
+            
+            // Shifting and combining
             val = (buf[0]) << 4 | (buf[1] >> 4);
+            //As a result of shifting and combining both bytes, we will get: 
             pc.printf("Val: 0x%x\n\r", val);
     
-            // Convert to 2's complement, since temperature can be negative
-            //val = 0xE70; // To check if it works. 0xE70 represents -25 degrees
+            // Negative temperatures
+            // 0xE70 represents -25 degrees
+            //val = 0xE70; // To check if it works uncomment the left side of this line.
             if ( val > 0x7FF ){
-                // It is the same to do val = (~val + 1)*(-1)
-                val |= 0xF000;
+                // Convert to 2's complement
+                val |= 0xF000; // This solution works just for 16 bits
+                // For 32 bits ==> val |= FFFFF000
+                // Don't forget to change the type of variable of "val", if you want to use 32 bits.
+                // General solution for any quantity of bytes: val = (~val + 1)*(-1)
+                // Now it is a int16_t variable.
             }
         
-            // Convert to float temperature value (Celsius)
+            // Temperature value in bytes needs to be converted to float temperature value (in Celsius)
+            // In this case, as we are using 12 bits of resolution, just a 0.0625 scale factor is required
+            // IMPORTANT: To get the maximum of accuracy, that is 0.0625 degrees, 0.0625 scale factor is required, 
+            // therefore 12 bits of resolution are required. 
+            // However, the more resolution is not involved to the more temperature accuracy.
+            // That means that if you are using 12 bits, 0.0625 scale factor is required, 
+            // but for example 0.5 degrees of accuracy can be obtained if the sensor is configured.
+            // IMPORTANT: So, resolution is related with temperature accuracy. But your sensor have to be configured.
             temp_c = val * 0.0625;
             pc.printf("Temperature: %.2f ºC\n\r", temp_c);
             
